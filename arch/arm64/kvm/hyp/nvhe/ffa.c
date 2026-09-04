omajili-manbu/Xiaomi_Rodin_Kernel_Enhance

FROMLIST: KVM: arm64: Zero out the stack initialized data in the FFA handler
Don't leak hypervisor stack data when using the FFA_VERSION call.
When the compiler doesn't support -ftrivial-auto-var-init=zero option
we need to zero out the stack initialized variable before returning data
to the host caller.

Closes:
https://lore.kernel.org/all/20260616160016.C62C81F000E9@smtp.kernel.org/

Bug: 503389212
Reported-by: Sashiko AI <sashiko-bot@kernel.org>
Fixes: c9c012625e12 ("KVM: arm64: Trap FFA_VERSION host call in pKVM")
Reviewed-by: Vincent Donnefort <vdonnefort@google.com>
Link: https://lore.kernel.org/all/20260702103848.1647249-7-sebastianene@google.com/
Change-Id: I31a99e4ecae50155b94b49858c267397e3b5ab7d
Signed-off-by: Sebastian Ene <sebastianene@google.com>

arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -816,7 +816,7 @@ bool kvm_host_ffa_handler(struct kvm_cpu_context *ctxt, u32 func_id)
 	DECLARE_REG(u64, arg2, ctxt, 2);
 	DECLARE_REG(u64, arg3, ctxt, 3);
 	DECLARE_REG(u64, arg4, ctxt, 4);
-	struct arm_smccc_res res;
+	struct arm_smccc_res res = {0};
 	bool handled = true;
 	int err = 0;
 

CodeLinaro-mirror/la_kernel_msm

Change-Id: I425d811f13b0e9ba74b59ba59da0a6afe576341d
(cherry picked from commit 09e239e51cc44dd320187c7e7d8cab58bd7bfaff)
Signed-off-by: Lee Jones <joneslee@google.com>
(cherry picked from commit 702117ce26666b071ed42d808a91b67fd7fbb67a)

arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1625,7 +1625,7 @@ static int kvm_host_ffa_signal_availability(void)
 
 bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt, u32 func_id)
 {
-	struct arm_smccc_1_2_regs res;
+	struct arm_smccc_1_2_regs res = {0};
 	int ret;
 
 	/*
@@ -1742,7 +1742,7 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 {
 	struct kvm_vcpu *vcpu = &hyp_vcpu->vcpu;
 	struct kvm_cpu_context *ctxt = &vcpu->arch.ctxt;
-	struct arm_smccc_1_2_regs res;
+	struct arm_smccc_1_2_regs res = {0};
 	struct kvm_hyp_req *req;
 	int ret;
 